c05637074dd0a5ec4192787a8b4a29cd0e8e682e
xusoo <josepmc90@gmail.com>
2019-10-18 22:04:30 +0200

    Set up global theme and support dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,40 @@ import 'package:simple_todo/widgets/tasks_list.dart';
 
 void main() => runApp(MyApp());
 
+const materialColor = Colors.deepOrange;
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var primaryColor = Colors.deepOrange;
-
     return ChangeNotifierProvider<TasksModel>(
       builder: (context) => TasksModel(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: primaryColor,
+          brightness: Brightness.light,
+          primarySwatch: materialColor,
+          disabledColor: Colors.grey.shade100,
+          appBarTheme: AppBarTheme(
+            textTheme: TextTheme(
+              title: TextStyle(color: Colors.black38),
+            ),
+          ),
           buttonTheme: ButtonThemeData(
-            buttonColor: primaryColor,
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
             textTheme: ButtonTextTheme.primary,
+          ),
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: materialColor,
+          primaryColor: materialColor,
+          accentColor: materialColor,
+          toggleableActiveColor: materialColor,
+          appBarTheme: AppBarTheme(
+            textTheme: TextTheme(
+              title: TextStyle(color: Colors.black38),
+            ),
           ),
         ),
         home: HomePage(),
@@ -31,8 +51,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tasks'),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(width: 1, color: Colors.black38)),
+          ),
+          child: AppBar(
+            elevation: 0,
+            centerTitle: false,
+            title: Padding(
+              padding: const EdgeInsets.only(top: 25.0),
+              child: Text('Tasks', style: TextStyle(fontSize: 32)),
+            ),
+          ),
+        ),
       ),
       body: TasksList(),
     );
